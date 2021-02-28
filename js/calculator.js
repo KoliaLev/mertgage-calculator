9f8b6a51b2009f4c5225504287f18a0bcf7c0c37
KoliaLev <kolialevkovich@gmail.com>
2021-02-28 14:00:33 +0200

    рефакторинг, проверки на ввод значений в формы

--- js/calculator.js
+++ js/calculator.js
@@ -32,22 +32,27 @@ function addBank(event) {
   event.preventDefault();
   const bank = {};
 
-  let bankName = document.getElementById("bank_name");
-  let interestRate = document.getElementById("interest_rate");
-  let maxLoan = document.getElementById("maximum_loan");
-  let minDownPayment = document.getElementById("minimum_down_payment");
-  let loanTerm = document.getElementById("loan_term");
+  let bankName = formForAddBank.bank_name;
+  let interestRate = formForAddBank.interest_rate;
+  let maxLoan = formForAddBank.maximum_loan;
+  let minDownPayment = formForAddBank.minimum_down_payment;
+  let loanTerm = formForAddBank.loan_term;
 
-  if (
-    !bankName.value.trim() ||
-    !interestRate.value.trim() ||
-    !maxLoan.value.trim() ||
-    !minDownPayment.value.trim() ||
-    !loanTerm.value.trim()
-  ) {
-    showNote(bankName, "не все поля заполнены");
+  if (!bankName.value.trim()) {
+    showNote(bankName, "введите название банка");
   } else if (isBank(bankName.value)) {
     showNote(bankName, "такой банк уже есть в базе");
+  } else if (isNaN(interestRate.value.trim()) || !interestRate.value.trim()) {
+    console.log(interestRate.value.trim());
+    showNote(interestRate, "введите число");
+  } else if (isNaN(maxLoan.value.trim()) || !maxLoan.value.trim()) {
+    showNote(maxLoan, "введите число");
+  } else if (isNaN(minDownPayment.value.trim()) || !minDownPayment.value.trim()) {
+    showNote(minDownPayment, "введите число");
+  } else if (+interestRate.value.trim() >= 100) {
+    showNote(interestRate, "проц ставка слижком большая");
+  } else if (+minDownPayment.value.trim() >= 100) {
+    showNote(minDownPayment, "мин сума взноса больше 100%");
   } else {
     bank.bankName = bankName.value.trim();
     bank.interestRate = interestRate.value.trim();
@@ -165,13 +170,22 @@ function addButton(elem, action) {
       // превращаем ячейки строки на инпуты
       let nameBank = button.parentElement.parentElement.firstElementChild.innerText;
       let lineCells = button.parentElement.parentElement.getElementsByTagName("td");
-      for (let cell = 0; cell < 5; cell++) {
-        text = lineCells[cell].innerText;
-        console.log(text + " переданый текс в инпут ");
-        lineCells[cell].innerHTML = `
-        <input type="text" value="${text}" style="padding: 0px; margin: 0px;" /> 
-        `;
-      }
+
+      lineCells[0].innerHTML = `<input type="text" value="${lineCells[0].innerText}" style="padding: 0px; margin: 0px;" /> `;
+      lineCells[1].innerHTML = `<input type="number" value="${lineCells[1].innerText}" style="padding: 0px; margin: 0px;" /> `;
+      lineCells[2].innerHTML = `<input type="number" value="${lineCells[2].innerText}" style="padding: 0px; margin: 0px;" /> `;
+      lineCells[3].innerHTML = `<input type="number" value="${lineCells[3].innerText}" style="padding: 0px; margin: 0px;" /> `;
+      lineCells[4].innerHTML = `<input type="number" value="${lineCells[4].innerText}" style="padding: 0px; margin: 0px;" /> `;
+
+      ///
+      // for (let cell = 0; cell < 5; cell++) {
+      //   text = lineCells[cell].innerText;
+      //   console.log(text + " переданый текс в инпут ");
+      //   lineCells[cell].innerHTML = `
+      //   <input type="text" value="${text}" style="padding: 0px; margin: 0px;" />
+      //   `;
+      // }
+      ///
 
       lineCells[5].firstChild.hidden = true; // скрываем кнопку 'edit'
 
@@ -182,15 +196,17 @@ function addButton(elem, action) {
       lineCells[5].append(buttonSave);
 
       buttonSave.onclick = () => {
-        lineCells[5].firstChild.hidden = false;
-        lineCells[5].lastElementChild.remove();
+        if (editBank(nameBank, lineCells)) {
+          for (let cell = 0; cell < 5; cell++) {
+            text = lineCells[cell].firstElementChild.value;
+            lineCells[cell].innerHTML = `${text}`;
+          }
 
-        for (let cell = 0; cell < 5; cell++) {
-          text = lineCells[cell].firstElementChild.value;
-          lineCells[cell].innerHTML = `${text}`;
+          lineCells[5].firstChild.hidden = false;
+          lineCells[5].lastElementChild.remove();
+        } else {
+          showNote(button, "нельзя оставлять яч пусые");
         }
-
-        editBank(nameBank, lineCells);
       };
     };
   }
@@ -218,35 +234,52 @@ function deleteBank(name) {
 
 function editBank(name, props) {
   let bank = banks.find((bank) => bank.bankName == name);
+  console.log(props);
+
+  props = Array.from(props)
+    .slice(0, 5)
+    .map((el) => el.firstElementChild.value);
+
+  console.log(props);
 
   console.log("передано имя банка: " + name);
   console.log("найден банк: " + bank.bankName);
 
-  bank.bankName = props[0].innerHTML;
-  bank.interestRate = props[1].innerHTML;
-  bank.maxLoan = props[2].innerHTML;
-  bank.minDownPayment = props[3].innerHTML;
-  bank.loanTerm = props[4].innerHTML;
+  if (props[0] && props[1] && props[2] && props[3] && props[4]) {
+    bank.bankName = props[0];
+    bank.interestRate = props[1];
+    bank.maxLoan = props[2];
+    bank.minDownPayment = props[3];
+    bank.loanTerm = props[4];
+
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function showNote(anchor, html) {
-  anchor.parentElement.style.position = "relative";
   let note = document.createElement("div");
   note.className = "note";
+  note.style.background = "white";
+  note.style.opacity = "0.9";
   note.innerHTML = html;
-  note.style.zIndex = "999";
-  note.style.position = "absolute";
+
   anchor.parentElement.append(note);
   anchor.style.background = "red";
 
   positionAt(anchor, note);
-  setInterval(() => {
+  const interval = setInterval(() => {
     note.remove();
-    anchor.style.background = "white";
+    anchor.style.background = "";
+    clearInterval(interval);
   }, 1500);
 
   function positionAt(anchor, elem) {
     // elem.style.left = anchorCoords.left + "px";
+    anchor.parentElement.style.position = "relative";
+    elem.style.position = "absolute";
+    elem.style.zIndex = "999";
     elem.style.top = anchor.parentElement.clientHeight + "px";
   }
 }
